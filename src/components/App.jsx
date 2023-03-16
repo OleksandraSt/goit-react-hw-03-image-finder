7d1d1db47939292b975ac1c7c3bf17b1be869147
OleksandraSt <communication1@ukr.net>
2023-03-16 18:25:36 +0200

    додає image-finder до дз3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,91 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Loader from './Loader';
+import SearchBar from './Searchbar/Searchbar';
+import pixabayApi from './api/api';
+import Button from './Button';
+import { Request, ErrorMessage } from './ImageGallery/ImageGallery.styled';
+
+export class App extends Component {
+  state = {
+    query: '',
+    images: [],
+    isLoading: false,
+    error: null,
+    page: 1,
+    total: 0,
+    status: 'idle',
+  };
+
+  handleFormSubmit = query => {
+    this.setState({ query, images: [], page: 1 });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      this.setState({ isLoading: true, error: null });
+      console.log('new request');
+      pixabayApi
+        .fetchImages(this.state.query, this.state.page)
+        .then(images =>
+          this.setState({
+            images: images.hits,
+            total: images.total,
+            status: 'resolved',
+          })
+        )
+        .catch(error => this.setState({ error }))
+        .finally(() => this.setState({ isLoading: false }));
+    }
+  }
+
+  loadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+    pixabayApi
+      .fetchImages(this.state.query, this.state.page)
+
+      .then(data => {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...data.hits],
+        }));
+      })
+      .catch(error => this.setState({ error }))
+      .finally(() => this.setState({ isLoading: false }));
+  };
+
+  render() {
+    const { isLoading, query, images, error, status, total } = this.state;
+    return (
+      <>
+        <SearchBar onSubmit={this.handleFormSubmit} />
+        {isLoading && <Loader />}
+        {!query && <Request>Enter a request</Request>}
+        {error && <ErrorMessage> {error.message}</ErrorMessage>}
+        {status === 'resolved' && <ImageGallery images={images} />}
+        {status === 'resolved' && total === 0 && (
+          <ErrorMessage>Nothing Found</ErrorMessage>
+        )}
+        {images.length >= 12 && <Button onClick={this.loadMore} />}
+      </>
+    );
+  }
 };
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
